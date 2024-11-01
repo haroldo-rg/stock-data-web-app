683dae1b204b8f457b4424c156c73b3a863a7801
Haroldo Gomes <haroldorg@gmail.com>
2024-10-31 21:47:48 -0300

    fix: change globalization config to pt-BR

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Carrega as configurações do arquivo appsettings.json
 builder.Configuration.AddConfiguration(ConfigurationLoader.LoadConfiguration());
+
+// Configuração de Globalização
+var defaultCulture = new CultureInfo("pt-BR");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+};
+
+// Configuração dos serviços
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = localizationOptions.DefaultRequestCulture;
+    options.SupportedCultures = localizationOptions.SupportedCultures;
+    options.SupportedUICultures = localizationOptions.SupportedUICultures;
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();  // Adiciona suporte a Razor Pages
@@ -20,6 +40,11 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseHttpsRedirection();
+
+// Aplicar a globalização
+app.UseRequestLocalization(localizationOptions);
 
 app.MapRazorPages();
 app.MapControllers();
